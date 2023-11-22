ed643c9225e58e155b01234eb2112d7dcd87f366
Nicola Tabai <nicola.tabai98@gmail.com>
2023-11-22 16:03:24 +0100

    Print array's elements and added link to bootstrap cdn

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-
     $hotels = [
-
         [
             'name' => 'Hotel Belvedere',
             'description' => 'Hotel Belvedere Descrizione',
@@ -37,7 +35,30 @@
             'vote' => 2,
             'distance_to_center' => 50
         ],
-
     ];
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    
+    <div class="container">
+    <?php
+        foreach($hotels as $hotel) {
+        // var_dump($hotel['name']);
+    ?>
+        <p><?php echo $hotel['name']?></p>
+    <?php
+    }
+     ?>
+    </div>
+     
+    
+   
+</body>
+</html>
